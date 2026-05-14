fc3e20ba7d0d860bb33ac4bd6a325d97af2ea053
PolyKybd <67008047+thpoll83@users.noreply.github.com>
2026-05-14 14:28:37 +0200

    fix to make qmk lint work on ci

--- keyboards/handwired/polykybd/rules.mk
+++ keyboards/handwired/polykybd/rules.mk
@@ -1,1 +1,3 @@
+DEFAULT_FOLDER = handwired/polykybd/split72
+
 SRC += side.c state.c split_sync.c multicore_exec.c hid_com.c fill_overlay.c polykybd.c fill_overlay.c poly_util.c matrix_helper.c bridge_helper.c
